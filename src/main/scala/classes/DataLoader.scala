7fb91a1b060db52698df9ba19da2f78230956c91
domino644 <cichemleko@gmail.com>
2024-06-13 00:11:56 +0200

    code refactor

--- src/main/scala/classes/DataLoader.scala
+++ src/main/scala/classes/DataLoader.scala
@@ -2,44 +2,43 @@ package agh.scala.footballanalyzer
 package classes
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+
 import scala.collection.mutable.ListBuffer
 
 
-
-class DataLoader(spark:SparkSession, competitionID:Int, seasonID:Int) {
-
+class DataLoader(spark: SparkSession, competitionID: Int, seasonID: Int) {
 
 
-    private def getMatchesID: Array[Long]  = {
+  private def getMatchesID: Array[Long] = {
 
-        val matchHolder = new MatchHolder(spark, competitionID = competitionID, seasonID = seasonID)
-        val allMatchID = matchHolder.getDF.select("match_id").collect()
-        val matchIDs = allMatchID.map(row => row.getLong(0))
+    val matchHolder = new MatchHolder(spark, competitionID = competitionID, seasonID = seasonID)
+    val allMatchID = matchHolder.getDF.select("match_id").collect()
+    val matchIDs = allMatchID.map(row => row.getLong(0))
 
-        println(matchIDs.mkString("Array(", ", ", ")"))
-        matchIDs
+    println(matchIDs.mkString("Array(", ", ", ")"))
+    matchIDs
 
-    }
-
-
-    def getAllEventsDF: DataFrame  = {
-        val matchesID = getMatchesID
-        val events = ListBuffer[DataFrame]()
+  }
 
 
-        for (matchID <- matchesID) {
+  def getAllEventsDF: DataFrame = {
+    val matchesID = getMatchesID
+    val events = ListBuffer[DataFrame]()
 
-            val eventHolder: EventHolder = new EventHolder(spark, matchID)
-            val eventDF = eventHolder.getDF
-            events += eventDF
 
-        }
+    for (matchID <- matchesID) {
 
-        val finalDF = events.reduce((df1, df2) => df1.unionByName(df2, allowMissingColumns = true))
-
-        finalDF
+      val eventHolder: EventHolder = new EventHolder(spark, matchID)
+      val eventDF = eventHolder.getDF
+      events += eventDF
 
     }
+
+    val finalDF = events.reduce((df1, df2) => df1.unionByName(df2, allowMissingColumns = true))
+
+    finalDF
+
+  }
 
 
 }
